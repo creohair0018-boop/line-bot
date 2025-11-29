c168169e06038f0c80ff08cdcae69943f0d9bc55
creohair0018-boop <creohair0018@gmail.com>
2025-11-29 19:30:30 +0900

    Update webhook.js

--- line-bot/api/webhook.js
+++ line-bot/api/webhook.js
@@ -1,27 +1,3 @@
-import { Client, middleware } from '@line/bot-sdk';
-
-const client = new Client({
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET
-});
-
-export default async function handler(req, res) {
-  console.log("CHANNEL_ACCESS_TOKEN:", process.env.CHANNEL_ACCESS_TOKEN ? "OK" : "NOT SET");
-  console.log("CHANNEL_SECRET:", process.env.CHANNEL_SECRET ? "OK" : "NOT SET");
-
-  if (req.method === 'POST') {
-    try {
-      // LINE Webhook ミドルウェアの検証
-      await middleware({
-        channelSecret: process.env.CHANNEL_SECRET
-      })(req, res, () => {});
-      console.log("Webhook received:", req.body);
-      res.status(200).send('OK');
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error');
-    }
-  } else {
-    res.status(200).send('GET OK');
-  }
+export default function handler(req, res) {
+  res.status(200).send('GET OK');
 }
